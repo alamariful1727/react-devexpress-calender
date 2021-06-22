9c576b81044ee5a0c311d21aff9c2855923d439a
Ariful Alam <alamariful1727@gmail.com>
2021-06-22 17:43:51 +0600

    calender refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import React from "react";
-import Compliance from "./calendar";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import Calendar from './calendar';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Compliance}></Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Calendar}></Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/calendar.tsx
+++ src/calendar.tsx
@@ -1,8 +1,5 @@
-// import { MenuItem } from "@material-ui/core";
-import React from "react";
-// import { useForm } from 'react-hook-form';
-// import { ReactHookFormCustomSelect } from '../../components/Shared/CustomSelect';
-import { ViewState } from "@devexpress/dx-react-scheduler";
+import React, { useState } from 'react';
+import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   DayView,
@@ -12,165 +9,166 @@ import {
   DateNavigator,
   ViewSwitcher,
   TodayButton,
-  // Resources,
   AppointmentTooltip,
-} from "@devexpress/dx-react-scheduler-material-ui";
-import { withStyles, Theme, createStyles } from "@material-ui/core";
-import { blue, teal, yellow } from "@material-ui/core/colors";
-import Paper from "@material-ui/core/Paper";
-import { fade } from "@material-ui/core/styles/colorManipulator";
-import { WithStyles } from "@material-ui/styles";
-import classNames from "clsx";
+  Resources,
+  WeekView,
+} from '@devexpress/dx-react-scheduler-material-ui';
+import { withStyles, Theme, createStyles } from '@material-ui/core';
+import { blue, indigo, teal, yellow } from '@material-ui/core/colors';
+import Paper from '@material-ui/core/Paper';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { WithStyles } from '@material-ui/styles';
+import classNames from 'clsx';
 
 const appointments = [
   {
-    title: "Prepare 2015 Marketing Plan",
+    title: 'Prepare 2015 Marketing Plan',
     startDate: new Date(2021, 5, 25, 13, 0),
     endDate: new Date(2021, 5, 25, 13, 30),
-    // priority: 2,
-    // location: 'Room 3',
+    priority: 2,
+    location: 'Room 3',
   },
   {
-    title: "Brochure Design Review",
+    title: 'Brochure Design Review',
     startDate: new Date(2021, 5, 28, 14, 10),
     endDate: new Date(2021, 5, 28, 15, 30),
-    // priority: 1,
-    // location: 'Room 1',
+    priority: 1,
+    location: 'Room 1',
   },
   {
-    title: "Website Re-Design Plan",
+    title: 'Website Re-Design Plan',
     startDate: new Date(2021, 5, 29, 9, 30),
     endDate: new Date(2021, 5, 29, 11, 30),
-    // priority: 1,
-    // location: 'Room 3',
+    priority: 1,
+    location: 'Room 3',
   },
   {
-    title: "Book Flights to San Fran for Sales Trip",
+    title: 'Book Flights to San Fran for Sales Trip',
     startDate: new Date(2021, 6, 2, 12, 0),
     endDate: new Date(2021, 6, 2, 13, 0),
-    // priority: 3,
-    // location: 'Room 2',
+    priority: 3,
+    location: 'Room 2',
   },
   {
-    title: "Install New Router in Dev Room",
+    title: 'Install New Router in Dev Room',
     startDate: new Date(2021, 6, 2, 14, 30),
     endDate: new Date(2021, 6, 2, 15, 30),
-    // priority: 2,
-    // location: 'Room 3',
+    priority: 2,
+    location: 'Room 3',
   },
   {
-    title: "Approve Personal Computer Upgrade Plan",
+    title: 'Approve Personal Computer Upgrade Plan',
     startDate: new Date(2021, 6, 4, 10, 0),
     endDate: new Date(2021, 6, 4, 11, 0),
-    // priority: 1,
-    // location: 'Room 1',
+    priority: 1,
+    location: 'Room 1',
   },
   {
-    title: "Final Budget Review",
+    title: 'Final Budget Review',
     startDate: new Date(2021, 6, 6, 12, 0),
     endDate: new Date(2021, 6, 6, 13, 35),
-    // priority: 3,
-    // location: 'Room 1',
+    priority: 3,
+    location: 'Room 1',
   },
   {
-    title: "New Brochures",
+    title: 'New Brochures',
     startDate: new Date(2021, 6, 6, 14, 30),
     endDate: new Date(2021, 6, 6, 15, 45),
-    // priority: 3,
-    // location: 'Room 3',
+    priority: 3,
+    location: 'Room 3',
   },
   {
-    title: "Install New Database",
+    title: 'Install New Database',
     startDate: new Date(2021, 6, 10, 9, 45),
     endDate: new Date(2021, 6, 10, 11, 15),
-    // priority: 2,
-    // location: 'Room 2',
+    priority: 2,
+    location: 'Room 2',
   },
   {
-    title: "Approve New Online Marketing Strategy",
+    title: 'Approve New Online Marketing Strategy',
     startDate: new Date(2021, 6, 12, 12, 0),
     endDate: new Date(2021, 6, 12, 14, 0),
-    // priority: 1,
-    // location: 'Room 2',
+    priority: 1,
+    location: 'Room 2',
   },
   {
-    title: "Upgrade Personal Computers",
+    title: 'Upgrade Personal Computers',
     startDate: new Date(2021, 6, 16, 15, 15),
     endDate: new Date(2021, 6, 16, 16, 30),
-    // priority: 2,
-    // location: 'Room 3',
+    priority: 2,
+    location: 'Room 3',
   },
   {
-    title: "Customer Workshop",
+    title: 'Customer Workshop',
     startDate: new Date(2021, 6, 18, 11, 0),
     endDate: new Date(2021, 6, 18, 12, 0),
-    // priority: 3,
-    // location: 'Room 1',
+    priority: 3,
+    location: 'Room 1',
   },
   {
-    title: "Prepare 2015 Marketing Plan",
+    title: 'Prepare 2015 Marketing Plan',
     startDate: new Date(2021, 6, 20, 11, 0),
     endDate: new Date(2021, 6, 20, 13, 30),
-    // priority: 1,
-    // location: 'Room 3',
+    priority: 1,
+    location: 'Room 3',
   },
   {
-    title: "New Brochures",
+    title: 'New Brochures',
     startDate: new Date(2021, 6, 23, 14, 30),
     endDate: new Date(2021, 6, 23, 15, 45),
-    // priority: 2,
-    // location: 'Room 3',
+    priority: 2,
+    location: 'Room 3',
   },
   {
-    title: "Install New Database",
+    title: 'Install New Database',
     startDate: new Date(2021, 6, 23, 9, 45),
     endDate: new Date(2021, 6, 23, 11, 15),
-    // priority: 3,
-    // location: 'Room 2',
+    priority: 3,
+    location: 'Room 2',
   },
   {
-    title: "Approve New Online Marketing Strategy",
+    title: 'Approve New Online Marketing Strategy',
     startDate: new Date(2021, 6, 26, 12, 0),
     endDate: new Date(2021, 6, 26, 14, 0),
-    // priority: 1,
-    // location: 'Room 1',
+    priority: 1,
+    location: 'Room 1',
   },
   {
-    title: "Upgrade Personal Computers",
+    title: 'Upgrade Personal Computers',
     startDate: new Date(2021, 6, 31, 15, 15),
     endDate: new Date(2021, 6, 31, 16, 30),
-    // priority: 2,
-    // location: 'Room 3',
+    priority: 2,
+    location: 'Room 3',
   },
   {
-    title: "Install New Database",
+    title: 'Install New Database',
     startDate: new Date(2021, 6, 31, 9, 45),
     endDate: new Date(2021, 6, 31, 11, 15),
-    // priority: 3,
-    // location: 'Room 2',
+    priority: 3,
+    location: 'Room 2',
   },
 ];
 
-// const resources = [
-//   {
-//     fieldName: 'location',
-//     title: 'Location',
-//     instances: [
-//       { id: 'Room 1', text: 'Room 1', color: indigo },
-//       { id: 'Room 2', text: 'Room 2', color: blue },
-//       { id: 'Room 3', text: 'Room 3', color: teal },
-//     ],
-//   },
-//   {
-//     fieldName: 'priority',
-//     title: 'Priority',
-//     instances: [
-//       { id: 1, text: 'High Priority', color: teal },
-//       { id: 2, text: 'Medium Priority', color: blue },
-//       { id: 3, text: 'Low Priority', color: indigo },
-//     ],
-//   },
-// ];
+const resources = [
+  {
+    fieldName: 'location',
+    title: 'Location',
+    instances: [
+      { id: 'Room 1', text: 'Room 1', color: indigo },
+      { id: 'Room 2', text: 'Room 2', color: blue },
+      { id: 'Room 3', text: 'Room 3', color: teal },
+    ],
+  },
+  {
+    fieldName: 'priority',
+    title: 'Priority',
+    instances: [
+      { id: 1, text: 'High Priority', color: teal },
+      { id: 2, text: 'Medium Priority', color: blue },
+      { id: 3, text: 'Low Priority', color: indigo },
+    ],
+  },
+];
 
 const styles = ({ palette }: Theme) =>
   createStyles({
@@ -189,10 +187,10 @@ const styles = ({ palette }: Theme) =>
     },
     weekEndCell: {
       backgroundColor: fade(palette.action.disabledBackground, 0.04),
-      "&:hover": {
+      '&:hover': {
         backgroundColor: fade(palette.action.disabledBackground, 0.04),
       },
-      "&:focus": {
+      '&:focus': {
         backgroundColor: fade(palette.action.disabledBackground, 0.04),
       },
     },
@@ -200,135 +198,115 @@ const styles = ({ palette }: Theme) =>
       backgroundColor: fade(palette.action.disabledBackground, 0.06),
     },
     text: {
-      overflow: "hidden",
-      textOverflow: "ellipsis",
-      whiteSpace: "nowrap",
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
+      whiteSpace: 'nowrap',
     },
     content: {
       opacity: 0.7,
     },
     container: {
-      width: "100%",
+      width: '100%',
       lineHeight: 1.2,
-      height: "100%",
+      height: '100%',
     },
   });
 
-type AppointmentProps = Appointments.AppointmentProps &
-  WithStyles<typeof styles>;
-type AppointmentContentProps = Appointments.AppointmentContentProps &
-  WithStyles<typeof styles>;
-type TimeTableCellProps = MonthView.TimeTableCellProps &
-  WithStyles<typeof styles>;
-type DayScaleCellProps = MonthView.DayScaleCellProps &
-  WithStyles<typeof styles>;
+type AppointmentProps = Appointments.AppointmentProps & WithStyles<typeof styles>;
+type AppointmentContentProps = Appointments.AppointmentContentProps & WithStyles<typeof styles>;
+type TimeTableCellProps = MonthView.TimeTableCellProps & WithStyles<typeof styles>;
+type DayScaleCellProps = MonthView.DayScaleCellProps & WithStyles<typeof styles>;
 
-const isWeekEnd = (date: Date): boolean =>
-  date.getDay() === 0 || date.getDay() === 6;
-const defaultCurrentDate = new Date(2021, 6, 2, 11, 15);
+const isWeekEnd = (date: Date): boolean => date.getDay() === 0 || date.getDay() === 6;
 
-const DayScaleCell = withStyles(styles)(
-  ({ startDate, classes, ...restProps }: DayScaleCellProps) => (
-    <MonthView.DayScaleCell
-      className={classNames({
-        [classes.weekEndDayScaleCell]: isWeekEnd(startDate),
-      })}
-      startDate={startDate}
-      {...restProps}
-    />
-  )
-);
+const DayScaleCell = withStyles(styles)(({ startDate, classes, ...restProps }: DayScaleCellProps) => (
+  <MonthView.DayScaleCell
+    className={classNames({
+      [classes.weekEndDayScaleCell]: isWeekEnd(startDate),
+    })}
+    startDate={startDate}
+    {...restProps}
+  />
+));
 
-const TimeTableCell = withStyles(styles)(
-  ({ startDate, classes, ...restProps }: TimeTableCellProps) => (
-    <MonthView.TimeTableCell
-      className={classNames({
-        [classes.weekEndCell]: isWeekEnd(startDate!),
-      })}
-      startDate={startDate}
-      {...restProps}
-    />
-  )
-);
+const TimeTableCell = withStyles(styles)(({ startDate, classes, ...restProps }: TimeTableCellProps) => (
+  <MonthView.TimeTableCell
+    className={classNames({
+      [classes.weekEndCell]: isWeekEnd(startDate!),
+    })}
+    startDate={startDate}
+    {...restProps}
+  />
+));
 
-const Appointment = withStyles(styles)(
-  ({ classes, data, ...restProps }: AppointmentProps) => (
-    <Appointments.Appointment
-      {...restProps}
-      className={classNames({
-        [classes.highPriorityAppointment]: data.priority === 1,
-        [classes.mediumPriorityAppointment]: data.priority === 2,
-        [classes.lowPriorityAppointment]: data.priority === 3,
-        [classes.appointment]: true,
-      })}
-      data={data}
-    />
-  )
-);
+const Appointment = withStyles(styles)(({ classes, data, ...restProps }: AppointmentProps) => (
+  <Appointments.Appointment
+    {...restProps}
+    className={classNames({
+      [classes.highPriorityAppointment]: data.priority === 1,
+      [classes.mediumPriorityAppointment]: data.priority === 2,
+      [classes.lowPriorityAppointment]: data.priority === 3,
+      [classes.appointment]: true,
+    })}
+    data={data}
+  />
+));
 
-// #FOLD_BLOCK
-const AppointmentContent = withStyles(styles, { name: "AppointmentContent" })(
-  ({
-    classes,
-    data,
-    ...restProps
-  }: // #FOLD_BLOCK
-  AppointmentContentProps) => {
-    // let priority = 'low';
-    // if (data.priority === 2) priority = 'medium';
-    // if (data.priority === 3) priority = 'high';
+const AppointmentContent = withStyles(styles, { name: 'AppointmentContent' })(
+  ({ classes, data, ...restProps }: AppointmentContentProps) => {
+    let priority = 'low';
+    if (data.priority === 2) priority = 'medium';
+    if (data.priority === 3) priority = 'high';
     return (
       <Appointments.AppointmentContent {...restProps} data={data}>
         <div className={classes.container}>
           <div className={classes.text}>{data.title}</div>
-          {/* <div className={classNames(classes.text, classes.content)}>{`Priority: ${priority}`}</div>
-          <div className={classNames(classes.text, classes.content)}>{`Location: ${data.location}`}</div> */}
-          {/* <div className={classNames(classes.text, classes.content)}>{`Name: ${'Fahim'}`}</div>
-          <h1 className="text-xl">Fahim</h1> */}
+          <div className={classNames(classes.text, classes.content)}>{`Advisor: ${'Fahim'}`}</div>
+          <div className={classNames(classes.text, classes.content)}>{`Priority: ${priority}`}</div>
+          <div className={classNames(classes.text, classes.content)}>{`Location: ${data.location}`}</div>
         </div>
       </Appointments.AppointmentContent>
     );
-  }
+  },
 );
 
-const Compliance = () => {
-  //   const { control } = useForm({
-  //     defaultValues: { reporter: 'year' },
-  //   });
-  return (
-    <div>
-      <div className="pl-2 mb-6">
-        <p className="mb-2">Duration</p>
-      </div>
+type viewNameType = 'work-week' | 'Week' | 'Month' | 'Day';
 
+const Calendar = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [currentViewName, setCurrentViewName] = useState<viewNameType>('Month');
+
+  const onCurrentDateChange = (newDate: Date) => setCurrentDate(newDate);
+  const onCurrentViewNameChange = (viewName: string) => setCurrentViewName(viewName as viewNameType);
+
+  return (
+    <div className="my-6 px-6 space-y-6">
+      <h1 className="font-semibold text-2xl">Calendar</h1>
+      <p className="font-semibold">Current date: {currentDate.toString()}</p>
+      <p className="font-semibold">Current view: {currentViewName}</p>
       <div className="bg-gray-50 rounded-3xl border border-gray-200 text-janttNavyBlue p-6">
-        <p className="font-semibold text-xl md:text-2xl">Compliance Calendar</p>
         <Paper>
           <Scheduler data={appointments}>
-            <ViewState defaultCurrentDate={defaultCurrentDate} />
-
-            <MonthView
-              dayScaleCellComponent={DayScaleCell}
-              timeTableCellComponent={TimeTableCell}
-            />
-            <DayView
-              displayName={"Week"}
-              startDayHour={9}
-              endDayHour={17}
-              intervalCount={7}
+            <ViewState
+              currentDate={currentDate}
+              currentViewName={currentViewName}
+              onCurrentDateChange={onCurrentDateChange}
+              onCurrentViewNameChange={onCurrentViewNameChange}
             />
 
-            <Appointments
-              appointmentComponent={Appointment}
-              appointmentContentComponent={AppointmentContent}
-            />
-            {/* <Resources data={resources} /> */}
+            <WeekView startDayHour={10} endDayHour={19} />
+            <WeekView name="work-week" displayName="Work Week" excludedDays={[0, 6]} startDayHour={9} endDayHour={19} />
+            <MonthView dayScaleCellComponent={DayScaleCell} timeTableCellComponent={TimeTableCell} />
+            <DayView startDayHour={10} endDayHour={24} />
+
+            <Appointments appointmentComponent={Appointment} appointmentContentComponent={AppointmentContent} />
+            <Resources data={resources} />
 
             <AppointmentTooltip showCloseButton />
             <Toolbar />
             <DateNavigator />
-            <ViewSwitcher />
             <TodayButton />
+            <ViewSwitcher />
           </Scheduler>
         </Paper>
       </div>
@@ -336,4 +314,4 @@ const Compliance = () => {
   );
 };
 
-export default Compliance;
+export default Calendar;
